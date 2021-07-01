7fff83c24a078b97cf9fd28e644b5947dfbca018
JingleMeow <tongmh2006@gmail.com>
2021-07-01 18:30:17 -0400

    Add routing to book list and set it as default.

--- BookStore/src/app/app-routing.module.ts
+++ BookStore/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
+import { BookListComponent } from './components/book-list/book-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/booklist', pathMatch: 'full' },
+  { path: 'booklist', component: BookListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule { };
+export const routingComponents = [BookListComponent];
--- BookStore/src/app/app.module.ts
+++ BookStore/src/app/app.module.ts
@@ -1,19 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MatDividerModule } from '@angular/material/divider';
 import { MatTableModule } from '@angular/material/table';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { BookListComponent } from './components/book-list/book-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookListComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
