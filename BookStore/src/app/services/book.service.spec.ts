c3d74394ac431ae3bb054dfab1e391721a6d5c84
JingleMeow <tongmh2006@gmail.com>
2021-07-01 21:33:26 -0400

    Add a unit test for adding a new book.

--- BookStore/src/app/services/book.service.spec.ts
+++ BookStore/src/app/services/book.service.spec.ts
@@ -1,16 +1,25 @@
+import { Book } from './../model/book';
 import { TestBed } from '@angular/core/testing';
 
 import { BookService } from './book.service';
 
 describe('BookService', () => {
   let service: BookService;
+  const testBook = "TestBook";
+  const testAuthor = "TestAuthor";
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
     service = TestBed.inject(BookService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+  it('should add a book successfully', () => {
+    let booksBefore = service.getAllBooks();
+    service.addBook(testBook, testAuthor);
+    let booksAfter = service.getAllBooks();
+    expect(booksAfter.length).toEqual(booksBefore.length + 1);
+    let newBook = booksAfter[booksAfter.length - 1];
+    expect(newBook.title).toEqual(testBook);
+    expect(newBook.author).toEqual(testAuthor);
+  })
 });
